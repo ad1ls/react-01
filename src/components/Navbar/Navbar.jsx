4170c4a3845f278db9e2c5a278cc033127072dc7
ad1ls <adilv022@gmail.com>
2020-03-03 14:58:18 +0300

    Add Modul "react-router dom"
    If you want download this app and you also want that this app will be working without any problems > install this modul. Open terminal and just write "npm install"

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     return (
         <div className={s.NavbarJsx}>
             <ul>
-                <li>Profile</li>
-                <li>Messages</li>
-                <li>Photos</li>
-                <li>Music</li>
-                <li>Settings</li>
+                <li><a href='/profile'>Profile</a></li>
+                <li><a href='/dialogs'>Messages</a></li>
+                <li><a href='#'>Photos</a></li>
+                <li><a href='#'>Music</a></li>
+                <li><a href='#'>Settings</a></li>
             </ul>
         </div>
     )
