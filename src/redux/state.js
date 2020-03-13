13d4ba56862ddb81ecec66efc8f19efcea537021
ad1ls <adilv022@gmail.com>
2020-03-13 23:20:39 +0300

    Add object store.
    Next station ---> OOP

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,67 +1,69 @@
-let rerenderEntireTree = () => {
+const store = {
 
-}
+    let rerenderEntireTree = () => {
 
-let state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Naruto best', likesCount: 2 },
-            { id: 2, message: 'Sasuke best ttt', likesCount: 1 }
-        ],
-        newPostText: 'KAVO'
     },
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'Adil' },
-            { id: 2, name: 'Ali' },
-            { id: 3, name: 'Artur' },
-            { id: 4, name: 'Ivan' },
-            { id: 5, name: 'Alina' },
-        ],
-        messages: [
-            { id: 1, message: 'Hi' },
-            { id: 2, message: 'Ypo' },
-            { id: 3, message: 'EWewewewe' }
-        ],
-        newMessageText: 'Smth'
+
+    let state = {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Naruto best', likesCount: 2 },
+                { id: 2, message: 'Sasuke best ttt', likesCount: 1 }
+            ],
+            newPostText: 'KAVO'
+        },
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: 'Adil' },
+                { id: 2, name: 'Ali' },
+                { id: 3, name: 'Artur' },
+                { id: 4, name: 'Ivan' },
+                { id: 5, name: 'Alina' },
+            ],
+            messages: [
+                { id: 1, message: 'Hi' },
+                { id: 2, message: 'Ypo' },
+                { id: 3, message: 'EWewewewe' }
+            ],
+            newMessageText: 'Smth'
+        },
+        sidebar: {}
     },
-    sidebar: {}
-};
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state)
-};
+    export const addPost = () => {
+        let newPost = {
+            id: 5,
+            message: state.profilePage.newPostText,
+            likesCount: 0
+        };
+        state.profilePage.posts.push(newPost);
+        state.profilePage.newPostText = '';
+        rerenderEntireTree(state)
+    },
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state)
+    export const updateNewPostText = (newText) => {
+        state.profilePage.newPostText = newText;
+        rerenderEntireTree(state)
+    },
+
+    export const subscribe = (observer) => {
+        rerenderEntireTree = observer;
+    },
+
+    export const addMessage = () => {
+        let newMessage = {
+            id: 4,
+            message: state.dialogsPage.newMessageText,
+
+        };
+        state.dialogsPage.messages.push(newMessage);
+        state.dialogsPage.newMessageText = '';
+        rerenderEntireTree(state)
+    },
+
+    export const addNewMessage = (newMessage) => {
+        state.dialogsPage.newMessageText = newMessage;
+        rerenderEntireTree(state)
+    }
 }
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export const addMessage = () => {
-    let newMessage = {
-        id: 4,
-        message: state.dialogsPage.newMessageText,
-
-    };
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.newMessageText = '';
-    rerenderEntireTree(state)
-}
-
-export const addNewMessage = (newMessage) => {
-    state.dialogsPage.newMessageText = newMessage;
-    rerenderEntireTree(state)
-}
-
 export default state;
